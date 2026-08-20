0214998a5d1174c4fb45a03f5ac8555afb70d742
mersad-zfo <mersad.ziro@gmail.com>
2026-08-20 12:16:56 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 importScripts('./app-notif-db.js');
 importScripts('./app-notif-shared.js');
 
-const CACHE_NAME = 'lifyar-v117';
+const CACHE_NAME = 'lifyar-v118';
 const ASSETS = [
   './',
   './index.html',
